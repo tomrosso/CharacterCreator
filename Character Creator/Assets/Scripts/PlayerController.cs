9750decc08b19f011a7379ba06d79c1bbf3fc54e
tomrosso <trosso2024@chadwickschool.org>
2021-12-09 11:44:11 -0800

    Weapon shop

--- Character Creator/Assets/Scripts/PlayerController.cs
+++ Character Creator/Assets/Scripts/PlayerController.cs
@@ -4,11 +4,20 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
+    public SpriteRenderer ArmorRenderer;
+    public SpriteRenderer WeaponRenderer;
+
    public void SetArmor(Sprite newSprite)
    {
        ArmorRenderer.sprite=newSprite;
    }
-    public SpriteRenderer ArmorRenderer;
+
+public void SetWeapon(Sprite newSprite)
+   {
+       WeaponRenderer.sprite=newSprite;
+   }
+    
     // Start is called before the first frame update
     void Start()
     {
